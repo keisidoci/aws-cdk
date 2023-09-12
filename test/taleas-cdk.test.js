7f04c251ca7a1bf410a9873014321057744a3555
keisidoci <keisidoci12@gmail.com>
2023-09-12 11:51:58 +0200

    Initial commit

---
+++ test/taleas-cdk.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const TaleasCdk = require('../lib/taleas-cdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/taleas-cdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new TaleasCdk.TaleasCdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
